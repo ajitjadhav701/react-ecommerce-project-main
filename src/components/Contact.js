9948f24a954abc2cd61037648c4ed9d079b1e554
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-05-23 13:00:07 +0530

    done with homepage layout part

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper>
+    <div className="section-center">
+      <h3>Join our news letter and 25% off</h3>
+      <div className="content">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eos earum dicta veritatis porro consectetur adipisci cum aut dolorem non.</p>
+        <form  className="contact-form">
+          <input type="email" className='form-input'placeholder='enter an email' />
+          <button type='submit' className='submit-btn'>Subscribe</button>
+        </form>
+        </div>
+    </div>
+  </Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
