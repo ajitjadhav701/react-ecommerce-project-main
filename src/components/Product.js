c6d84a398f057e799800dadd8735818159dcd7c9
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-05-23 18:18:13 +0530

    done with fetching products data and display on home page featured products section

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,8 +4,18 @@ import { formatPrice } from '../utils/helpers'
 import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
-const Product = () => {
-  return <h4>product</h4>
+const Product = ({image,name,price,id}) => {
+  return <Wrapper>
+    <div className="container">
+      <img src={image} alt='featured product image'/>
+      <Link to={`/products/${id}`} clasName='link'><FaSearch/></Link>
+
+    </div>
+    <footer>
+      <h5>{name}</h5>
+      <p>${price}</p>
+    </footer>
+  </Wrapper>
 }
 
 const Wrapper = styled.article`
